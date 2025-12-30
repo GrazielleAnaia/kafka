a2a53eaa117765ffdedc7ae45422bada1c003caf
Grazielle <grazielle.anaia@gmail.com>
2025-12-30 13:04:57 -0500

    added configuration of spring cloud stream function for consumer and producer

--- consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -3,22 +3,22 @@ package com.demo.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "my-topic", groupId = "my-group1")
-    public void listen(String message) {
-        System.out.println("Received message1: " + message);
-    }
-
-    @KafkaListener(topics = "my-topic", groupId = "my-group2")
-    public void listen2(String message) {
-        System.out.println("Received message2: " + message);
-    }
-
-    @KafkaListener(topics = "my-topic-rider", groupId = "my-group-rider")
-    public void listenRiderLocation(RiderLocation location) {
-        System.out.println("Received message from location ID: " + location.getRiderId() +
-                " latitude: " + location.getLatitude() + " longitude: " + location.getLongitude());
-    }
+//    @KafkaListener(topics = "my-topic", groupId = "my-group1")
+//    public void listen(String message) {
+//        System.out.println("Received message1: " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic", groupId = "my-group2")
+//    public void listen2(String message) {
+//        System.out.println("Received message2: " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic-rider", groupId = "my-group-rider")
+//    public void listenRiderLocation(RiderLocation location) {
+//        System.out.println("Received message from location ID: " + location.getRiderId() +
+//                " latitude: " + location.getLatitude() + " longitude: " + location.getLongitude());
+//    }
 }
